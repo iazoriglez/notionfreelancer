90e9bc1337758f08e3bc1076052b16e97b01fd87
iaglez <iazoriglez@gmail.com>
2026-06-27 13:20:03 +0200

    fix: remove sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://notionfreelancer.com',
   integrations: [
     mdx(),
-    sitemap(),
   ],
   i18n: {
     defaultLocale: 'es',
